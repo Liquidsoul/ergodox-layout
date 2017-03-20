5427d1094ba86018fd172a45aa81e7ce60f4e00d
Liquidsoul <liquidsoul@liquidsoul.fr>
2017-03-20 22:30:24 +0100

    Remove AZERTY_SYMB layer

--- layout/keymap.c
+++ layout/keymap.c
@@ -1,4 +1,3 @@
-// French AZERTY version of the default_osx file
 #include "ergodox.h"
 #include "debug.h"
 #include "action_layer.h"
@@ -7,8 +6,7 @@
 #define QWERTY 0 // qwerty keys
 #define QWERTY_SYMB 1 // qwerty symbols
 #define AZERTY 2 // azerty layer
-#define AZERTY_SYMB 3 // symbols
-#define MDIA 4 // media keys
+#define MDIA 3 // media keys
 
 /*
  * Note that the layouts in comments are for AZERTY OSX keyboard configuration.
@@ -44,69 +42,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [AZERTY] = KEYMAP(
         // left hand
         FR_MINS,                 FR_AMP,         FR_EACU,  FR_QUOT, FR_APOS, FR_LPRN, KC_LEFT,
-        KC_TAB,                  FR_A,           FR_Z,     KC_E,    KC_R,    KC_T,    TG(AZERTY_SYMB),
+        KC_TAB,                  FR_A,           FR_Z,     KC_E,    KC_R,    KC_T,    KC_TRNS,
         KC_ESC,                  FR_Q,           KC_S,     KC_D,    KC_F,    KC_G,
         KC_LSFT,                 FR_W,           KC_X,     KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
-        LT(AZERTY_SYMB,FR_UGRV), CTL_T(FR_LESS), KC_LALT,  KC_LGUI, FR_AT,
+        FR_UGRV, CTL_T(FR_LESS), KC_LALT,  KC_LGUI, FR_AT,
                      KC_LALT, KC_LGUI,
-                              MO(AZERTY_SYMB),
+                              KC_TRNS,
             KC_BSPC, KC_DELT, KC_LCTRL,
 
         // right hand
         KC_RGHT,         FR_SECT, FR_EGRV,         FR_EXLM, FR_CCED, FR_AGRV,           FR_RPRN,
-        TG(AZERTY_SYMB), KC_Y,    KC_U,            KC_I,    KC_O,    KC_P,              FR_GRV,
+        KC_TRNS, KC_Y,    KC_U,            KC_I,    KC_O,    KC_P,              FR_GRV,
                          KC_H,    KC_J,            KC_K,    KC_L,    FR_M,              KC_LGUI,
         MEH_T(KC_NO),    KC_N,    FR_COMM,         FR_SCLN, FR_COLN, CTL_T(FR_EQL),     KC_RSFT,
-                                  S(LALT(FR_AMP)), FR_GRV,  KC_LBRC, LT(MDIA, KC_RBRC), MO(AZERTY_SYMB),
+                                  S(LALT(FR_AMP)), FR_GRV,  KC_LBRC, LT(MDIA, KC_RBRC), KC_TRNS,
             KC_LALT,  CTL_T(KC_ESC),
             KC_LGUI,
             MO(MDIA), KC_ENT, KC_SPC
     ),
-/* Keymap: Symbol Layer
- *********************************************************************************************************************
- *                                                                                                                   *
- * .--------+------+------+------+------+------+------.         .------+------+------+------+------+------+--------. *
- * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |         |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  | *
- * +--------+------+------+------+------+------+------+         +------+------+------+------+------+------+--------+ *
- * |        |   <  |   >  |   {  |   }  |   |  |      |         |      |  UP  |   7  |   8  |   9  |   *  |   F12  | *
- * +--------+------+------+------+------+------+      |         |      +------+------+------+------+------+--------+ *
- * |        |   -  |   $  |   (  |   )  |   `  +------+         +------+ DOWN |   4  |   5  |   6  |   +  |        | *
- * +--------+------+------+------+------+------+      |         |      +------+------+------+------+------+--------+ *
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |         |      |   &  |   1  |   2  |   3  |   /  |        | *
- * '-+------+------+------+------+------+------+------'         '------+------+------+------+------+------+------+-' *
- *   |      |      |      |      |      |                                     |   0  |   ,  |      |   -  |      |   *
- *   '------+------+------+------+------'                                     '------+------+------+------+------'   *
- *                                       .------+------.       .------+------.                                       *
- *                                       |      |      |       |      |      |                                       *
- *                                .------+------+------+       +------+------+------.                                *
- *                                |      |      |      |       |      |      |      |                                *
- *                                |      |      +------+       +------+      |      |                                *
- *                                |      |      |      |       |      |      |      |                                *
- *                                '------+------+------'       '------+------+------'                                *
- *                                                                                                                   *
- *********************************************************************************************************************
- */
-// SYMBOLS
-[AZERTY_SYMB] = KEYMAP(
-       // left hand
-       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
-       KC_TRNS, FR_AT,   FR_HASH, FR_LCBR, FR_RCBR, FR_PIPE, KC_TRNS,
-       KC_TRNS, FR_MINS, FR_DLR,  FR_LPRN, FR_RPRN, FR_GRV,
-       KC_TRNS, FR_PERC, FR_CIRC, FR_LBRC, FR_RBRC, FR_TILD, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                           KC_TRNS, KC_TRNS,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
-       // right hand
-       KC_TRNS, KC_F6,   KC_F7, KC_F8,   KC_F9, KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_P7, KC_P8,   KC_P9, KC_PAST, KC_F12,
-                KC_DOWN, KC_P4, KC_P5,   KC_P6, KC_PPLS, KC_TRNS,
-       KC_TRNS, FR_AMP,  KC_P1, KC_P2,   KC_P3, KC_PSLS, KC_TRNS,
-                         KC_P0, KC_PDOT, KC_P0, KC_PMNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
-),
 /* Keymap: Basic QWERTY layer
 *********************************************************************************************************************
 *                                                                                                                   *
@@ -246,8 +199,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(AZERTY_SYMB),                // FN1 - Momentary Layer 1 (Symbols)
-    [2] = ACTION_LAYER_TAP_TOGGLE(MDIA),                // FN2 - Momentary Layer 2 (Media)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -283,8 +234,6 @@ void matrix_scan_user(void) {
       // TODO: Make this relevant to the ErgoDox EZ.
         case QWERTY:
             break;
-        case AZERTY_SYMB:
-            ergodox_right_led_3_on();
         case QWERTY_SYMB:
             ergodox_right_led_1_on();
             break;
