016539df9ef41ef71b7c51a2a90ac23d83721ed9
Liquidsoul <liquidsoul@liquidsoul.fr>
2017-04-02 18:18:20 +0200

    Use local setup through Brew instead of Docker and add interactive flashing step

--- Brewfile
+++ Brewfile
@@ -1,1 +1,9 @@
-brew 'docker'
+brew 'teensy_loader_cli'
+
+tap 'px4/px4'
+brew 'gcc-arm-none-eabi-49'
+
+tap 'osx-cross/avr'
+brew 'avr-libc'
+brew 'dfu-programmer'
+brew 'dfu-util'
--- buildLayout.sh
+++ buildLayout.sh
@@ -9,6 +9,47 @@ readonly LAYOUT_NAME=$1
 readonly KEYBOARD=$2
 readonly SUBPROJECT=$3
 
+function build {
+    local target=${1:-}
+    local master=${2:-left}
+    make ${KEYBOARD:?}-${SUBPROJECT:?}-${LAYOUT_NAME:?}-${target} master=${master}
+}
+
+function flashEZ {
+    echo "Flashing ez..."
+    echo "Plug the keyboard in reset mode. Press enter key to continue"
+    read x
+    teensy_loader_cli -mmcu=atmega32u4 -w ${KEYBOARD}_${SUBPROJECT}_${LAYOUT_NAME}.hex
+}
+
+function flashInfinity {
+    echo "Flashing infinity..."
+    echo "Plug the Left hand in reset mode. Press enter key to continue"
+    read x
+    build dfu-util left
+    echo "Plug the right hand in reset mode. Press enter key to continue"
+    read x
+    build dfu-util right
+}
+
+function flash {
+    echo "Flash keyboard? (y/N)"
+    read x
+    if [[ "$x" == "y" ]]; then
+        case ${SUBPROJECT} in
+            "ez")
+                flashEZ
+                ;;
+            "infinity")
+                flashInfinity
+                ;;
+            *)
+                echo "No implemented flashing method for subproject ${SUBPROJECT}"
+                ;;
+        esac
+    fi
+}
+
 pushd ${BUILD_FOLDER:?} > /dev/null
-docker run -e keymap=${LAYOUT_NAME:?} -e subproject=${SUBPROJECT:?} -e keyboard=${KEYBOARD:?} --rm -v $('pwd'):/qmk:rw edasque/qmk_firmware
+build && flash
 popd > /dev/null
